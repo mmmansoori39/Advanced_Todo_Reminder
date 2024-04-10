f30dc475ee4a8481ee5a06d56cabce3c721c711c
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2024-04-10 22:33:36 +0530

    fifth commit

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import userRoute from './routes/users.js'
 import taskRoute from './routes/tasks.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import './db/conn.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -19,8 +25,14 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
 app.use('/', userRoute);
-app.use('/', taskRoute)
+app.use('/', taskRoute);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+  })
 
 app.get('/', (req, res) => {
     res.send("hello")
